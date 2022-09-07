e867ec011d74dc0b1bb52336aed31e7776e52b11
purewack <54288625+purewack@users.noreply.github.com>
2022-09-07 13:09:26 +0100

    audioio: more specs timing and range

--- audioio/audioio.ino
+++ audioio/audioio.ino
@@ -9,6 +9,13 @@
 //ADC smpr_41_5 = 197.53kHz = 6.32spl / srate
 //ADC smpr_28_5 = 260.16kHz = 8.32spl / srate
 
+//ADC 4096 theoretical dynamic range = 72dB
+//pDAC 2048 theoretical dynamic range = 66dB
+//r2rDAC potentially problematic updating two GPIO registers
+//possibly DMA periph increment %2
+
+//srate 64 block expiry in 2ms
+
 void setup(){
     //setup uart
 
